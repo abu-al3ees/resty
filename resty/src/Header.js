81f430ab8fb53a5d6e48c56b74429f927e72fcb2
abu-al3ees <malkhazali83@gmail.com>
2021-06-13 20:33:46 +0300

    lab26

---
+++ resty/src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../src/style/Header.scss';
+
+const Header = ()=>{
+    return(
+        <header>
+          <div><h1>RESTy</h1></div>
+        
+      </header>
+    )
+}
+export default Header;
