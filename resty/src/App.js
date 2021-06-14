397cbef7ee154484538938b9695cf1a350222973
abu-al3ees <malkhazali83@gmail.com>
2021-06-14 17:56:55 +0300

    lab27

--- resty/src/App.js
+++ resty/src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
 import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
+import Form from './Form';
+//import Footer from './Footer';
+import Result from './Result';
 
-function App() {
-  return (
-    <React.Fragment>
-    <Header/>
-    <Main/>
-    <Footer/>
-  </React.Fragment>
-  );
+class App extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        data : [],
+        headers: {},
+          count: 0
+      };
+  }
+
+  handleForm = (data, headers) => {
+      this.setState({
+        data: data.results,
+         count:data.count,
+         headers:headers
+        });
+  }
+
+  render() {
+      return (
+          <>
+           <Header/>
+  
+ 
+              <Form handler={this.handleForm}/>
+              <Result results={this.state}/> 
+          </>
+      )
+  }
 }
+
+
+
 
 export default App;
