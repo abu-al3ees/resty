39d8217ddddfad379aa82d35f02cd0de3f92ab3c
abu-al3ees <malkhazali83@gmail.com>
2021-06-23 20:33:03 +0300

    cc

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,28 @@
 import React from 'react';
-import '../src/style/Header.scss';
-import {Link} from 'react-router-dom';
-const Header = ()=>{
-    return(
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './style/Header.scss';
+const Header=()=>{
+    return (
         <header>
-          <div><h1>RESTy</h1></div>
-          
-        
-      </header>
+            <h1>Resty</h1>
+
+            <nav>
+       <ul>
+        <li>
+           <Link exact  to="/">Home</Link>
+        </li>
+        <li>
+           <Link to="/history">History</Link>
+        </li>
+        <li>
+           <Link to="/help">Help</Link>
+        </li>
+       </ul>
+    </nav>
+
+        </header>
     )
 }
+
 export default Header;
--- src/main.js
+++ src/main.js
@@ -1,1 +1,28 @@
 import React from 'react';
+
+
+
+import Home from '../home/home.js';
+import Help from '../help/help.js';
+import History from '../historyPage/historyPage';
+//import { BrowserRouter} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+const Main = () =>{
+   return (
+     <>
+    
+    <Router>
+             <Route exact path="/" component={Home}  />
+             <Route path="/help" component={Help} />
+             <Route path="/history" component={History} />
+     </Router>
+     
+     </>
+
+
+   )
+
+}
+
+export default Main ;
