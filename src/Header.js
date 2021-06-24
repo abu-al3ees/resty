eabd4f4b4c0febcf59ca5595dee4f6ffbc54e983
abu-al3ees <malkhazali83@gmail.com>
2021-06-24 22:29:21 +0300

    deploy

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+//import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './style/Header.scss';
 const Header=()=>{
     return (
@@ -10,13 +10,13 @@ const Header=()=>{
             <nav>
        <ul>
         <li>
-           <Link exact  to="/">Home</Link>
+           <a href="/">Home</a>
         </li>
         <li>
-           <Link to="/history">History</Link>
+           <a href="/history">History</a>
         </li>
         <li>
-           <Link to="/help">Help</Link>
+           <a href="/help">Help</a>
         </li>
        </ul>
     </nav>
--- src/components/history.js
+++ src/components/history.js
@@ -1,19 +1,64 @@
+
 import React from 'react';
-import '../style/history.scss';
 
-function History(props){
+let resu=localStorage.getItem('headers');
+         let object=JSON.parse(resu);
 
+         if(!object){
+         object=[{url:'no url yet',
+         method:'no method'
+        }]
+         }
+
+function History({ props }) {
     return (
-      <div class="history">
-        <ul>
-          {props.history.map((item, idx) => 
-            <li key={idx}>
-              <span className={`method ${item.method}`}>{`${item.method}`}</span>
-              <button className='url-history'>{`${item.url}`}</button>
-            </li>
-          )}
-        </ul>
-      </div>
-    )
-}
-export default History;
+      <section id="history">
+        <table>
+          <thead>
+            <tr>
+              <th className="fwidth">Method</th>
+              <th>URL</th>
+            </tr>
+          </thead>
+          <tbody>
+              
+            {object.map((val, i) => {
+              return (
+                <tr
+                  key={i + val.method + val.url}
+                 
+                  onClick={historyClick}
+                >
+                  <th className="fwidth" id={`his${val.method}`}>{val.method}</th>
+                  <td>{val.url}</td>
+                  <td style={{ display: 'none' }}>{val.body}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </section>
+    );
+  }
+  
+  export default History;
+  
+  function historyClick(e) {
+    let method = e.currentTarget.childNodes[0].innerHTML;
+    let url = e.currentTarget.childNodes[1].innerHTML;
+    let body = e.currentTarget.childNodes[2].innerHTML;
+  console.log(method)
+  console.log(url)
+    const input = document.getElementById('inputUrl');
+    console.log(input)
+    input.value = url;
+ 
+    const button = document.getElementById(`${method}`)
+    console.log(button)
+  button.click();
+   
+  
+    const text = document.getElementById('body');
+   
+    text.innerHTML = body;
+  }
